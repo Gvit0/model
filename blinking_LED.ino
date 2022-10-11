8b74677938fec53b757e406d3b42bf4a15f40353
Gvit0 <90312305+Gvit0@users.noreply.github.com>
2022-10-11 11:22:46 +0300

    Update blinking_LED.ino

--- blinking_LED.ino
+++ blinking_LED.ino
@@ -1,11 +1,12 @@
-int led = 13;
-
+int led = 13; #пин со светодиодом
+int tempd = 10; #сколько раз в секунду
 void setup() {
-  pinMode(led,);
- 
-
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
-
+  digitalWrite(led, 1);
+  delay(1000 / tempd);
+  digitalWrite(led, 0);
+  delay(1000 / tempd);
 }
