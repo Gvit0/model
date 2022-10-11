1269edaa4071cb76a634d65accd46fbb6fef94a8
Gvit0 <90312305+Gvit0@users.noreply.github.com>
2022-10-11 13:23:55 +0300

    Update blinking_LED_pot.ino

--- blinking_LED_pot.ino
+++ blinking_LED_pot.ino
@@ -1,1 +1,17 @@
+const int ledPin =  13;      // пин со светодиодом
+int p;
  
+void setup() {
+  pinMode(led, OUTPUT);     
+  pinMode(A0, INPUT);
+}
+ 
+void loop()
+{
+  p = analogRead(A0);
+  p = map(p, 0, 255, 100, 2000);
+  digitalWrite(led, 1);
+  delay(p);
+  digitalWrite(led, 0);
+  delay(p);
+} 
