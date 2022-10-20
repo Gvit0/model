3dfa5210458fd35fa43acc2b2ad6452bcb5ce315
Gvit0 <90312305+Gvit0@users.noreply.github.com>
2022-10-20 21:27:47 +0300

    Update svetofor.ino

--- svetofor.ino
+++ svetofor.ino
@@ -1,21 +1,21 @@
 void setup()
 {
   pinMode(2, OUTPUT);
-  pinMode(4, OUTPUT);
   pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(2, HIGH);
-  delay(1500);
+  delay(1500); // Wait for 1500 millisecond(s)
   digitalWrite(2, LOW);
-  digitalWrite(4, HIGH);
-  delay(1500);
-  digitalWrite(4, LOW);
   digitalWrite(3, HIGH);
-  delay(1500);
+  delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  delay(1500); // Wait for 1500 millisecond(s)
+  digitalWrite(4, LOW);
   digitalWrite(2, HIGH);
-  delay(1500);
+  delay(1500); // Wait for 1500 millisecond(s)
 }
