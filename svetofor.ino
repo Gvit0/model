cbc15458abbed652642bde3c62d8d6b7408b32fe
Gvit0 <90312305+Gvit0@users.noreply.github.com>
2022-10-31 12:52:21 +0300

    Update svetofor.ino

--- svetofor.ino
+++ svetofor.ino
@@ -1,24 +1,34 @@
+// C++ code
+//
 void setup()
 {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(2, OUTPUT); //устанавливаем 2 пин (красный цвет) как выход 
+  pinMode(3, OUTPUT); //устанавливаем 3 пин (жёлтый цвет) как выход
+  pinMode(4, OUTPUT);//устанавливаем 4 пин (зелёный цвет) как выход
 }
 
 void loop()
 {
-  digitalWrite(2, HIGH);
-  delay(1500); // Wait for 1500 millisecond(s)
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(3, LOW);
-  digitalWrite(4, HIGH);
-  delay(1500); // Wait for 1500 millisecond(s)
+  digitalWrite(2, HIGH); // включаем красный цвет
+  delay(3000); //задержка 3 секунды
+  digitalWrite(2, LOW); // выключаем красный цвет
+  digitalWrite(4, HIGH); // включаем  зелёный цвет
+  delay(3000); //задержка 3 секунды
+  digitalWrite(4, LOW); // выключаем зелёный цвет
+  delay(500); 
+  digitalWrite(4, HIGH); 
+  delay(500);
   digitalWrite(4, LOW);
+  delay(500);
+  digitalWrite(4, HIGH);
+  delay(500);
+  digitalWrite(4, LOW);
+  delay(500);
+  digitalWrite(4, HIGH);
+  delay(500);
+  digitalWrite(4, LOW);
+  delay(500);
   digitalWrite(3, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(3, LOW)
-  digitalWrite(2, HIGH);
-  delay(1500); // Wait for 1500 millisecond(s)
+  delay(1000);
+  digitalWrite(3, LOW);
 }
