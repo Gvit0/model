ee2b4cbaa4a2c4a07a352386c77cdd18fed9973f
Gvit0 <90312305+Gvit0@users.noreply.github.com>
2022-12-02 08:30:32 +0300

    Update PWM_buthon.ino

--- ШИМ/PWM_buthon.ino
+++ ШИМ/PWM_buthon.ino
@@ -1,1 +1,20 @@
-
+int ledPin = 11;      // Присваиваем имя ledPin цифровому выводу 11, к которому подключен светодиод
+int brightness =0;
+int fadeValue = 5;    // шаг изменения яркости
+int buttonPlus=2;     //  Номер Pin к которому подключена кнопка "+" 
+int buttonMinus=3;    //  Номер Pin к которому подключена кнопка "-"
+ 
+void setup()  { 
+pinMode(ledPin, OUTPUT);
+} 
+void loop()  {
+  if (digitalRead(buttonPlus) == HIGH) {
+    brightness +=  fadeValue;
+  }
+  if (digitalRead(buttonMinus) == HIGH) {
+    brightness -=  fadeValue;
+  }
+  brightness = constrain(brightness, 0, 255);
+  analogWrite(ledPin, brightness);
+  delay(50);
+}
